982681cd334c063d62b2748b66c89ce93bcfa1c4
Umayr <Umayrkaleem@gmail.com>
2022-05-31 16:42:16 -0500

    added a test car and started arrow key functionality to control movement

---
+++ controls.js
@@ -0,0 +1,47 @@
+class Controls{
+    constructor(){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        this.#addKeyboardListeners();
+    }
+
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.up=true;
+                    break
+                case "ArrowDown":
+                    this.down=true;
+                    break;
+            }
+            console.table(this);
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.up=false;
+                    break
+                case "ArrowDown":
+                    this.down=false;
+                    break;
+            }
+            console.table(this);
+        }
+    }
+}
