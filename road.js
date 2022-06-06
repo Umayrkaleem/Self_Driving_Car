07a82ca12b84a805f8e6be41b57477689438bd92
Umayr <Umayrkaleem@gmail.com>
2022-06-06 00:40:50 -0500

    remove additional lane, fix getLaneCenter method to place car in center of whatever chosen lane

--- road.js
+++ road.js
@@ -1,7 +1,7 @@
 /* eslint-disable require-jsdoc */
 // eslint-disable-next-line no-unused-vars
 class Road {
-  constructor(x, width, laneCount=4) {
+  constructor(x, width, laneCount=3) {
     this.x=x;
     this.width=width;
     this.laneCount=laneCount;
@@ -16,7 +16,8 @@ class Road {
 
   getLaneCenter(laneIndex) {
     const laneWidth=this.width/this.laneCount;
-    return this.left+laneWidth/2+laneIndex*laneWidth;
+    return this.left+laneWidth/2+
+      Math.min(laneIndex, this.laneCount-1)*laneWidth;
   }
 
   draw(ctx) {
